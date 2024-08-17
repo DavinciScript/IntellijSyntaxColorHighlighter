a6a353b3d25b9e684b4b56d6cdf34a256a6b98c3
Bozhidar <bobi@microweber.com>
2024-08-17 15:19:05 +0300

    update

--- src/main/java/com/bobicloudvision/MyLanguageLexerAdapter.java
+++
@@ -1,9 +0,0 @@
-package com.bobicloudvision;
-
-import com.intellij.lexer.FlexAdapter;
-
-public class MyLanguageLexerAdapter extends FlexAdapter {
-    public MyLanguageLexerAdapter() {
-        super(new MyLanguageLexer(null));  // Pass an instance of your generated lexer here
-    }
-}
